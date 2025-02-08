a406fde85388a8394c3bc7826afd8c862394e150
Jessica Chen <jesschen046@gmail.com>
2025-02-07 22:31:57 -0500

    updated again

--- milestone-1/sql/tables.sql
+++ milestone-1/sql/tables.sql
@@ -1,16 +1,19 @@
 DROP TABLE IF EXISTS account;
 DROP TABLE IF EXISTS transaction;
 DROP TABLE IF EXISTS pokemon_card;
-DROP TABLE IF EXISTS pokemon_type;
+DROP TABLE IF EXISTS weaknesses;
+DROP TABLE IF EXISTS resistances;
 DROP TABLE IF EXISTS ability;
 DROP TABLE IF EXISTS attacks;
 
 CREATE TABLE account
   ( 
      uid     DECIMAL(9, 0) NOT NULL PRIMARY KEY AUTO_INCREMENT, 
-     acc_name    VARCHAR(40), NOT NULL,
-     email    VARCHAR(255), NOT NULL
-     usr_password    VARCHAR(255), NOT NULL
+     user_name    VARCHAR(40), NOT NULL,
+     email    VARCHAR(255), NOT NULL,
+     usr_password    VARCHAR(255), NOT NULL,
+     bio VARCHAR(255),
+     pfp VARCHAR(255),
   ); 
 
 CREATE TABLE transaction
@@ -18,7 +21,7 @@ CREATE TABLE transaction
      tid    DECIMAL(9, 0) NOT NULL PRIMARY KEY AUTO_INCREMENT ,
      sender_id  DECIMAL(9, 0) NOT NULL,
      receiver_id DECIMAL(9, 0) NOT NULL,
-     card_id VARCHAR(20), NOT NULL
+     card_id VARCHAR(20), NOT NULL,
      tdate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
      FOREIGN KEY(sender_id) REFERENCES account(uid),
      FOREIGN KEY(receiver_id) REFERENCES account(uid),
@@ -45,15 +48,15 @@ CREATE TABLE pokemon_card
 CREATE TABLE weaknesses
   ( 
      type_name     VARCHAR(20) NOT NULL, 
-     weakness     VARCHAR(10), 
-     PRIMARY KEY (type_name, weakness)
+     weakness     VARCHAR(20) NOT NULL, 
+     PRIMARY KEY (type_name, weakness),
   ); 
 
-CREATE TABLE resistanaces
+CREATE TABLE resistances
   ( 
      type_name     VARCHAR(20) NOT NULL, 
      resistance    VARCHAR(20) NOT NULL,
-     PRIMARY KEY (type_name, resistance)
+     PRIMARY KEY (type_name, resistance),
   ); 
 
 CREATE TABLE attacks 
